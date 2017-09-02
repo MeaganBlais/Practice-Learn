bd18f1f88077e320f955defe5ad0a495a5c1d4d8
Meagan Blais <meaganblais@gmail.com>
2017-09-02 12:55:38 -0400

    adding interating through arrays

--- The_Web_Developer_Bootcamp/S11-Javascript-Arrays/list.js
+++ The_Web_Developer_Bootcamp/S11-Javascript-Arrays/list.js
@@ -12,14 +12,19 @@ while (input !== "quit") {
 
   //handle input
   if (input === "list") {
-    console.log(toDos);
+    toDos.forEach(function(toDo) {
+      console.log(toDo);
+    })
   } else if (input === "new") {
     //ask for new todos
     var newToDo = prompt("Enter new ToDo");
     //add todos array
     toDos.push(newToDo);
-  // } else {
-  //   prompt("I don't understand. Please see options listed")
+  } else if (input === "delete") {
+    var index = prompt("Enter index number");
+    toDos.splice(index, 1);
+    // } else {
+    //   prompt("I don't understand. Please see options listed")
   }
   //ask again for new input
   input = prompt("What would you like to do?");
