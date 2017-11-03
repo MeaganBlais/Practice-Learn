ddbddef339b63727e78f7fec1344657e9e9847cc
Meagan Blais <meaganblais@gmail.com>
2017-11-03 00:05:01 -0400

    color changer challenge

---
+++ The_Web_Developer_Bootcamp/S14 - Advanced DOM/colorChanger/script.js
@@ -0,0 +1,20 @@
+// alert("JS is connected")
+
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+var isPurple = false;
+
+button.addEventListener("click", changeColor)
+
+function changeColor() {
+  // // alert("clicked");
+  // if (isPurple) {
+  //   document.body.style.background = "white";
+  // } else {
+  //   document.body.style.background = "purple";
+  // }
+  // isPurple = !isPurple;
+
+  document.body.classList.toggle("purple");
+  
+};
