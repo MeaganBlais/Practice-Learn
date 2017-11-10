aa9fa0f66c89d0c39e7d4953a2a87b0ce022e7b2
Meagan Blais <meaganblais@gmail.com>
2017-11-09 20:04:18 -0500

    adding gameOver feature

--- The_Web_Developer_Bootcamp/S14-Advanced DOM/scoreKeeper/scoreKeeper.js
+++ The_Web_Developer_Bootcamp/S14-Advanced DOM/scoreKeeper/scoreKeeper.js
@@ -1,16 +1,33 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.getElementById("#p2");
-var p1View = document.querySelector("p1s");
+var p2Button = document.getElementById("p2");
+var p1View = document.querySelector("#p1s");
 var p2View = document.getElementById("p2s")
 var p1Score = 0;
 var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+
 
 p1Button.addEventListener("click", function() {
-  p1Score++;
-  p1View.textContent = p1Score;
+  if (!gameOver) {
+    p1Score++;
+    if(p1Score === winningScore) {
+      gameOver = true;
+    }
+    p1View.textContent = p1Score;
+  }
+  
 });
 
 p2Button.addEventListener("click", function() {
-  p2Score++;
-  p2View.textContent = p2Score;
+  if (!gameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      gameOver = true;
+    }
+    p2View.textContent = p2Score;
+
+  }
+
 });
